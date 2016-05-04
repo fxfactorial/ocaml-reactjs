88398cc9b123e1668b68b729f28b13e9da16fd72
Edgar Aroutiounian <edgar.factorial@gmail.com>
2016-05-03 19:40:03 -0700

    WIP This commit breaks merlin 2.3.1

--- src/react_js.ml
+++ src/react_js.ml
@@ -30,20 +30,26 @@ class component_spec
         (prev_props:Js.Unsafe.any ->
          prev_state:Js.Unsafe.any -> unit) option)
     ?(component_will_unmount : (unit -> unit) option)
-    ~render_f:(render_f : (unit -> react_element))
+    ~render_f:(render_f : (react_element -> react_element))
     ~display_name:(display_name : string)
     () = object(self)
 
-  val raw_js = object%js
+  val mutable raw_js = !!(object%js end)
 
+  initializer
+    self#_set_raw_js
+
+  method private _set_raw_js =
+    raw_js <- !!(object%js
     val displayName = Js.string display_name
     val propTypes = match prop_types with
         None -> Js.null | Some p -> !!p |> Js.Opt.return
-    val render =  !! !@(fun () ->
-        (render_f ())#unsafe_raw
-      )
-
-  end
+    val render = !!(Js.wrap_meth_callback (fun this ->
+        (new react_element this
+         |> render_f)
+        #unsafe_raw
+      ))
+      end)
 
   method unsafe_raw = raw_js
 
@@ -57,7 +63,15 @@ end
 
 and react_element (wrapped_elem : Js.Unsafe.any) = object
 
+  val mutable props_ : Js.Unsafe.any =
+    Js.Unsafe.get wrapped_elem "props"
+
   method unsafe_raw = wrapped_elem
+
+  method props = props_
+
+  method get key : Js.Unsafe.any =
+    Js.Unsafe.get props_ (Js.string key)
 
 end
 
@@ -88,14 +102,79 @@ let create_element :
 let clone_element ?children ?props (elem : react_element) =
   ()
 
-let create_factory
-    (elem : [`Html_elem of string |
-             `React_class of react_class]) =
-  ()
+
+let create_factory (class_ : react_class) :
+  < .. > Js.t option -> react_element =
+  let factory = react##createFactory class_#unsafe_raw in
+  fun props ->
+    (match props with
+     | None -> Js.Unsafe.fun_call factory [||]
+     | Some p -> Js.Unsafe.fun_call factory [|!!props|])
+    |> new react_element
 
 let is_valid_element obj = ()
 
-module DOM = struct end
+module DOM = struct
+
+  let handle = react##.DOM
+
+  let a ?(props : < .. > Js.t option) msg =
+    (handle##a
+       (match props with None -> Js.null | Some p -> p |> Js.Opt.return)
+       (Js.string msg))
+    |> new react_element
+
+  let abbr ?(props : < .. > Js.t option) msg =
+    (handle##abbr
+       (match props with None -> Js.null | Some p -> p |> Js.Opt.return)
+       (Js.string msg))
+    |> new react_element
+
+  let body ?(props : < .. > Js.t option) msg =
+    (handle##body
+       (match props with None -> Js.null | Some p -> p |> Js.Opt.return)
+       (Js.string msg))
+    |> new react_element
+
+  let code ?(props : < .. > Js.t option) msg =
+    (handle##code
+       (match props with None -> Js.null | Some p -> p |> Js.Opt.return)
+       (Js.string msg))
+    |> new react_element
+
+  let h1 ?(props : < .. > Js.t option) msg =
+    (handle##h1
+       (match props with None -> Js.null | Some p -> p |> Js.Opt.return)
+       (Js.string msg))
+    |> new react_element
+
+  let h2 ?(props : < .. > Js.t option) msg =
+    (handle##h2
+       (match props with None -> Js.null | Some p -> p |> Js.Opt.return)
+       (Js.string msg))
+    |> new react_element
+
+  let img ?(props : < .. > Js.t option) msg =
+    (handle##img
+       (match props with None -> Js.null | Some p -> p |> Js.Opt.return)
+       (Js.string msg))
+    |> new react_element
+
+  let p ?(props : < .. > Js.t option) msg =
+    (handle##p
+       (match props with None -> Js.null | Some p -> p |> Js.Opt.return)
+       (Js.string msg))
+    |> new react_element
+
+  let div ?(props : < .. > Js.t option) msg =
+    (handle##div
+       (match props with None -> Js.null | Some p -> p |> Js.Opt.return)
+       (Js.string msg))
+    |> new react_element
+
+
+
+end
 
 module PropTypes = struct end
 
@@ -120,27 +199,47 @@ module ReactDOM = struct
 
 end
 
-(* let () = *)
-(*   let commentbox = *)
-(*     new component_spec *)
-(*       ~display_name:"CommentBox" *)
-(*       ~render_f:(fun () -> *)
-(*           create_element *)
-(*             (New_elem {html_elem_t = "div"; *)
-(*                        class_name = "commentBox"; *)
-(*                        content = "Hello world, I'm a comment box"}) *)
-(*         ) *)
-(*       () *)
-(*     |> create_class *)
-(*   in *)
-(*   let comment_box_instance = *)
-(*     create_element (React_class commentbox) *)
-(*   in *)
-(*   match *)
-(*     ReactDOM.render *)
-(*       ~react_elem:comment_box_instance *)
-(*       ~on_update_or_render:(fun () -> print_endline "Called Render") *)
-(*       (Dom_html.getElementById "content") *)
-(*   with *)
-(*   | None -> print_endline "Couldn't render element" *)
-(*   | Some _ -> print_endline "Rendered!" *)
+let () =
+  let example_application =
+    new component_spec
+      ~display_name:"CommentBox"
+      ~render_f:(fun self ->
+          let elapsed : float =
+            Js.math##round
+              (Js.Unsafe.get self#props "elapsed" /. 100.0)
+          in
+          let seconds = elapsed /. 10.0 in
+          let message =
+            seconds
+            |> Printf.sprintf
+              "React has been successfully running for %f seconds."
+          in
+          DOM.p message
+        )
+      ()
+    |> create_class
+  in
+  let example_app_factory = create_factory example_application in
+  let start = (new%js Js.date_now)##getTime in
+  Printf.sprintf "%f" start
+  |> print_endline;
+  let a =
+    Dom_html.window##setInterval
+      !@(fun () ->
+          ReactDOM.render
+            (example_app_factory (object%js
+               val elapsed = new%js Js.date_now)##getTime -. start
+             end)
+        )
+    50.0
+  in
+  ()
+  (* match *)
+  (*   ReactDOM.render *)
+  (*     ~react_elem:(DOM.p "Hello world, in a p") *)
+  (*     (\* ~react_elem:comment_box_instance *\) *)
+  (*     ~on_update_or_render:(fun () -> print_endline "Called Render") *)
+  (*     (Dom_html.getElementById "content") *)
+  (* with *)
+  (* | None -> print_endline "Couldn't render element" *)
+  (* | Some _ -> print_endline "Rendered!" *)
