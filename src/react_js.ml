5a6f75516fd222dc4d47e53f7cbf55f6704671db
Edgar Aroutiounian <edgar.factorial@gmail.com>
2016-05-07 16:06:18 -0700

    Render to screen with new idea

--- src/react_js.ml
+++ src/react_js.ml
@@ -6,34 +6,74 @@ let (react, react_dom) =
 
 let version = Runa.(react <!> "version" |> Js.to_string)
 
-type this
-
 module React = struct
 
-  class react_class =
-    let () =
-      let obj = object%js
-        val hack = Js.wrap_meth_callback (fun this ->
-            let open Runa in
-            (match this <*> "other_value" with
-               None -> print_endline "couldn't pull value"
-             | Some s -> print_endline (Js.to_string s));
-            this
-          )
-      end
-      in
-      let obj2 = object%js
-        val other_value = Js.string "Hello world"
-      end
-      in
-      let obj_merged = Runa.merge obj obj2 in
-      Runa.debug obj_merged
+  type tag = Class | Bare
+
+  type component_spec = { initial_state : (Runa.this -> Js.Unsafe.any) option;
+                          default_props : (Runa.this -> Js.Unsafe.any) option;}
+
+  let simple_defaults =
+    { initial_state = None;
+      default_props = None; }
+
+  type new_elem = { elem_name : string;
+                    children : string; }
+
+  class react_element (arg : [`React_class of react_class
+                             | `New_elem of new_elem ]) =
+    let _elem_ = match arg with
+      | `React_class e ->
+        react##createElement e#unsafe_raw Js.null
+      | `New_elem {elem_name; children} ->
+        react##createElement
+          (Js.string elem_name)
+          (object%js end)
+          (Js.string children)
     in
     object
-      initializer
-        print_endline "Created element"
+      method unsafe_raw = Runa.(!!_elem_)
     end
 
+  and react_class
+      ?com_spec:(com_spec = (simple_defaults :  component_spec))
+      ~render:(render_f : Runa.this -> react_element)
+      ~display_name:d
+      extras
+    =
+    let _class_ =
+      let open Runa in
+      let obj_render = object%js
+        val displayName = Js.string d
+        val render =
+          (fun this -> (render_f this)#unsafe_raw)
+          |> Js.wrap_meth_callback
+        val getInitialState = Runa.when_f com_spec.initial_state
+        val getDefaultProps = Runa.when_f com_spec.default_props
+      end
+      in
+      let obj_merged =
+        (match extras with None -> object%js end | Some p -> object_of_table p)
+        |> merge obj_render
+      in
+      let class_ = react##createClass obj_merged in
+      class_
+    in
+    object
+      method tag = Class
+      method unsafe_raw = Runa.(!!_class_)
+    end
+
+  let create_class
+      ?com_spec:(com_spec = (simple_defaults :  component_spec))
+      ~render:(render : Runa.this -> react_element)
+      ~display_name:display_name
+      extras =
+    new react_class ~com_spec ~render ~display_name extras
+
+  let create_element (arg : [`React_class of react_class
+                            | `New_elem of new_elem ]) =
+    new react_element arg
 
   module DOM = struct
 
@@ -46,32 +86,35 @@ end
 module ReactDOM = struct
 
   let render
-      ?on_update_or_render:(cb : (this -> unit) option)
-      react_elem
+      ?on_update_or_render:(cb : (Runa.this -> unit) option)
+      (react_elem : React.react_element)
       (dom_elem : #Dom_html.element Js.t) =
     let cb_func = match cb with
         None -> Js.null
       | Some f ->
-        Js.wrap_meth_callback (fun this -> f this)
+        Runa.with_this f
         |> Js.Opt.return
     in
-    react_dom##render react_elem dom_elem cb_func
+    react_dom##render react_elem#unsafe_raw dom_elem cb_func
 
 end
 
-(* let _ = let open Runa in *)
-  (* let props = object%js val className = Js.string "commentBox" end in *)
-  (* let children = Js.string "Hello world" in *)
-  (* let elem = React.DOM.create ~props ~children `p in *)
-  (* ReactDOM.render *)
-  (*   ~on_update_or_render:(fun this -> *)
-  (*       match this <*> "className" with *)
-  (*       | None -> print_endline "No class name" *)
-  (*       | Some name -> log name *)
-  (*     ) *)
-  (*   elem *)
-  (*   (Dom_html.getElementById "content") *)
-
-let _ = let open Runa in
-  let e = new React.react_class in
-  ()
+let _ = let open React in
+  let open Runa in
+  let comment_box =
+    create_class
+      ~com_spec:{initial_state = Some (fun _ ->
+          !!(object%js val count = 0 end)
+        );
+         default_props = None;}
+      ~display_name:"Comment Box"
+      ~render:(fun this ->
+          `New_elem {elem_name = "p";
+                     children = "Hello World"}
+          |> create_element
+        )
+      None
+  in
+  ReactDOM.render
+    (create_element (`React_class comment_box))
+    (Dom_html.getElementById "content")
