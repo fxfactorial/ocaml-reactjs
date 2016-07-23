1c4f60068e92f06f678a238fb4588b221600d168
Edgar Aroutiounian <edgar.factorial@gmail.com>
2016-07-23 14:31:35 -0700

    Implemented Quadractic!

--- reactjs_based_examples/quadratic/example.ml
+++ reactjs_based_examples/quadratic/example.ml
@@ -1,110 +1,52 @@
-(* var QuadraticCalculator = React.createClass({ *)
-(*   getInitialState: function() { *)
-(*     return { *)
-(*       a: 1, *)
-(*       b: 3, *)
-(*       c: -4 *)
-(*     }; *)
-(*   }, *)
-
-(*   /** *)
-(*    * This function will be re-bound in render multiple times. Each .bind() will *)
-(*    * create a new function that calls this with the appropriate key as well as *)
-(*    * the event. The key is the key in the state object that the value should be *)
-(*    * mapped from. *)
-(*    */ *)
-(*   handleInputChange: function(key, event) { *)
-(*     var partialState = {}; *)
-(*     partialState[key] = parseFloat(event.target.value); *)
-(*     this.setState(partialState); *)
-(*   }, *)
-
-(*   render: function() { *)
-(*     var a = this.state.a; *)
-(*     var b = this.state.b; *)
-(*     var c = this.state.c; *)
-(*     var root = Math.sqrt(Math.pow(b, 2) - 4 * a * c); *)
-(*     var denominator = 2 * a; *)
-(*     var x1 = (-b + root) / denominator; *)
-(*     var x2 = (-b - root) / denominator; *)
-(*     return ( *)
-(*       <div> *)
-(*       <strong> *)
-(*       <em>ax</em><sup>2</sup> + <em>bx</em> + <em>c</em> = 0 *)
-(*       </strong> *)
-(*       <h4>Solve for <em>x</em>:</h4> *)
-(*       <p> *)
-(*       <label> *)
-(*       a: <input type="number" value={a} onChange={this.handleInputChange.bind(null, 'a')} /> *)
-(*       </label> *)
-(*       <br /> *)
-(*       <label> *)
-(*       b: <input type="number" value={b} onChange={this.handleInputChange.bind(null, 'b')} /> *)
-(*       </label> *)
-(*       <br /> *)
-(*       <label> *)
-(*       c: <input type="number" value={c} onChange={this.handleInputChange.bind(null, 'c')} /> *)
-(*       </label> *)
-(*       <br /> *)
-(*       x: <strong>{x1}, {x2}</strong> *)
-(*       </p> *)
-(*       </div> *)
-(*     ); *)
-(*   } *)
-(* }); *)
+open StdLabels
 
 let quadratic_calculator = Reactjs.(
     make_class_spec
-      ~initial_state:(fun ~this ->
-          object%js
-            val a = 1.0 val b = 3.0 val c = -3.0
-          end
-        )
+      ~initial_state:(fun ~this -> object%js
+                       val a = 1.0 val b = 3.0 val c = -3.0
+                     end)
       (fun ~this -> let open Infix in
         let handle_input_change =
           fun ~key event ->
             let new_state =
-              ([(key, event##.target##.value |> Js.parseFloat |> Js.number_of_float )] >>>
+              ([(key,
+                 event##.target##.value |> Js.parseFloat |> Js.number_of_float )] >>>
                object%js end)
             in
             this##setState new_state
         in
         let (a, b, c) = this##.state##.a, this##.state##.b, this##.state##.c in
         let root = Js.math##sqrt ((Js.math##pow b 2.0) -. 4.0 *. a *. c) in
-        let (denominator, x1, x2) = 2.0 *. a, -.b +. root, -.b -. root in
+        let denominator = 2.0 *. a in
+        let (x1, x2) = (-.b +. root) /. denominator, (-.b -. root) /. denominator in
+        let input_label ~key init_value = DOM.(
+            make ~tag:`label
+              [Text (Printf.sprintf "%s: " key);
+               Elem (make ~elem_spec:(object%js
+                       val type_ = Js.string "number"
+                       val value = !^init_value
+                       val onChange = handle_input_change ~key
+                     end) ~tag:`input [])]
+          )
+        in
+        let label_row l = l |> List.map ~f:(fun (key, value) ->
+            [Elem (input_label ~key value); Elem (DOM.make ~tag:`br [])]
+          ) |> List.flatten
+        in
+        let equation_row = DOM.(
+            [Elem (make ~tag:`em [Text "ax"]); Elem (make ~tag:`sup [Text "2"]);
+             Text " + "; Elem (make ~tag:`em [Text "bx"]); Text " + ";
+             Elem (make ~tag:`em [Text "c"]); Text " = 0"])
+        in
         DOM.(make ~tag:`div
-               [Elem (make ~tag:`strong
-                        [Elem (make ~tag:`em [Text "ax"]);
-                         Elem (make ~tag:`sup [Text "2"]);
-                         Text " + "; Elem (make ~tag:`em [Text "bx"]);
-                         Text " + "; Elem (make ~tag:`em [Text "c"]);
-                         Text " = 0"]);
+               [Elem (make ~tag:`strong equation_row );
                 Elem (make ~tag:`h4 [Text "Solve for ";
                                      Elem (make ~tag:`em [Text "x"])]);
-                Elem
-                  (make ~tag:`p
-                     [Elem
-                        (make ~tag:`label
-                           [Text "a: ";
-                            Elem
-                              (make
-                                 ~elem_spec:(object%js
-                                   val type_ = !*(string_of_float a)
-                                   val value = !^a
-                                   val onChange = handle_input_change ~key:"a"
-                                 end)
-                                 ~tag:`input [])])]);
-                Elem (
-                  make ~tag:`br []);
-                Elem (
-                  make
-                    ~elem_spec:(object%js
-                      val type_ = !*"number"
-                      val value = !^b
-                      val onChange = handle_input_change ~key:"b"
-                    end)
-                    ~tag:`label []);
-
+                Elem (make ~tag:`p
+                        (label_row [("a", a); ("b", b); ("c", c)] @
+                         [Text "x: ";
+                          Elem (make ~tag:`strong
+                                  [Text (Printf.sprintf "%f %f" x1 x2)])]))
                ]))
     |> create_class
   )
